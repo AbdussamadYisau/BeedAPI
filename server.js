0c85f9f0bdbc6af21bea8c850c0b8321b450b98f
AbdussamadYisau <sammieyisau@gmail.com>
2021-02-02 19:42:28 +0100

    Added fallback cors

--- server.js
+++ server.js
@@ -15,12 +15,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(cors());
 
-// app.use((req,res,next) => {
-//     res.setHeader('Access-Control-Allow-Origin', '*');
-//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
-//     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-//     next();
-// });
+app.use((req,res,next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
 
 // Routes
 app.use(auctionPost);
